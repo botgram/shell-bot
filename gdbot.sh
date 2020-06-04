a7f51dd8d846f0a39a1cf0131126c165c87669f4
cgking <58586625+cgkings@users.noreply.github.com>
2020-06-04 22:06:51 +0800

    Update gdbot.sh

--- gdbot.sh
+++ gdbot.sh
@@ -25,15 +25,15 @@ id=$link
 fi
 echo "==<<极速转存即将开始，可ctrl+c中途中断>>=="
 echo 【开始拷贝】......
-gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P --stats = 0.1m
+gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P --stats = 0.2m
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
 echo 【查缺补漏】......
-gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P --stats = 0.1m
+gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P --stats = 0.2m
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
 echo 【去重检查】......
 gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -q
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
 echo 【比对检查】......
 gclone check goog:{$link} goog:{myid}/"$rootName" --size-only --one-way --no-traverse --min-size 10M -q
-echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  检查OK，转存已完成"
+echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  检查完毕"
 ./gdbot.sh
