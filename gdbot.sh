f0a3eecc18ec42ddd94429b65a1caeef6c10da8b
cgking <58586625+cgkings@users.noreply.github.com>
2020-06-04 11:27:09 +0800

    Update gdbot.sh

--- gdbot.sh
+++ gdbot.sh
@@ -10,8 +10,6 @@ link=${link#*folders/};
 link=${link#*d/};
 link=${link%?usp*}
 s1=`gclone size cgkings:{"$link"} --min-size 10M 2>&1` 
-id=$link
-j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
     if [[ $s1 =~ "Error 404" ]]
     then
     echo "链接无效，检查是否有权限" && exit
@@ -21,6 +19,8 @@ j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) roo
           "file root name："$rootName""
     fi          
 fi
+id=$link
+    j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
 echo ==<<极速转存即将开始，可ctrl+c中途中断>>==
 echo 【开始拷贝】......
 p1=`gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -P -q`
@@ -35,17 +35,12 @@ echo 【比对ok，转存完毕】
 ./gdbot.sh
 else
 echo 【查缺补漏】......
-gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -P -q
+p2='gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -P -q'
+echo $p2
 echo 【去重检查】......
-gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -P -q
+d1='gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -P -q'
+echo $d1
 echo 【比对检查】......
 c4=`gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --min-size 10M --log-level NOTICE`
-c5=$(echo $c4 | grep 'differences'
-c6=$(echo $c5 | grep -Po '(?<="':")[^differences]*')
-    if $c6 = "0"
-    then
-    echo 【比对ok，转存完毕】
-    ./gdbot.sh
-    else
-    echo 【转存错误，请手动再次转存】
-    exit
+echo $c4
+./gdbot.sh
