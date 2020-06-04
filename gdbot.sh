65ebc42723a566393d458f468db8abab0a4dbdeb
cgking <58586625+cgkings@users.noreply.github.com>
2020-06-04 11:06:46 +0800

    Update gdbot.sh

--- gdbot.sh
+++ gdbot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-read -p """输入分享链接
-     请输入 =>:""" link
+read -p """请输入分享链接==》""" link
 # 检查接受到的分享链接规范性，并转化出分享文件ID
 if [ -z "$link" ] ;then
     echo "不允许输入为空"
@@ -20,35 +19,33 @@ j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) roo
     echo "分享链接的基本信息如下：
           "$s1"
           "file root name："$rootName""
+    fi          
 fi
-    ==<<极速转存即将开始，可ctrl+c中途中断>>=="
-    echo 【开始拷贝】......
-    gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -P -q
-    echo 【比对检查】......
-    c1=`gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --min-size 10M --log-level NOTICE`
-    c2=$(echo $c1 | grep 'differences')
-    c3=$(echo $c2 | grep -Po '(?<="':")[^differences]*')
-        if $c3 = "0"
-        then
-        echo 【比对ok，转存完毕】
-        ./gdbot.sh
-        else
-        echo 【查缺补漏】......
-        gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -P -q
-        echo 【去重检查】......
-        gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -P -q
-        echo 【比对检查】......
-        c4=`gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --min-size 10M --log-level NOTICE`
-        c5=$(echo $c4 | grep 'differences'
-        c6=$(echo $c5 | grep -Po '(?<="':")[^differences]*')
-            if $c6 = "0"
-            then
-            echo 【比对ok，转存完毕】
-            ./gdbot.sh
-            else
-            echo 【转存错误，请手动再次转存】
-            fi
-        fi
-    
-    
-    
+==<<极速转存即将开始，可ctrl+c中途中断>>=="
+echo 【开始拷贝】......
+p1=`gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -P -q`
+echo $p1
+echo 【比对检查】......
+c1=`gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --min-size 10M --log-level NOTICE`
+c2=$(echo $c1 | grep 'differences')
+c3=$(echo $c2 | grep -Po '(?<="':")[^differences]*')
+if $c3 = "0"
+then
+echo 【比对ok，转存完毕】
+./gdbot.sh
+else
+echo 【查缺补漏】......
+gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -P -q
+echo 【去重检查】......
+gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -P -q
+echo 【比对检查】......
+c4=`gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --min-size 10M --log-level NOTICE`
+c5=$(echo $c4 | grep 'differences'
+c6=$(echo $c5 | grep -Po '(?<="':")[^differences]*')
+    if $c6 = "0"
+    then
+    echo 【比对ok，转存完毕】
+    ./gdbot.sh
+    else
+    echo 【转存错误，请手动再次转存】
+    exit
