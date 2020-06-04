999bedbbffb5a4b1b10762c818919958b41e93e5
cgking <58586625+cgkings@users.noreply.github.com>
2020-06-04 19:35:52 +0800

    Update gdbot.sh

--- gdbot.sh
+++ gdbot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Author: cgking
+# Created Time : 2020.6.4
+# File Name: gdbot.sh
+# Description:
 read -p "请输入分享链接==>" link
 # 检查接受到的分享链接规范性，并转化出分享文件ID
 if [ -z "$link" ] ; then
@@ -8,9 +12,9 @@ link=${link#*id=};
 link=${link#*folders/};
 link=${link#*d/};
 link=${link%?usp*}
-check_results=`gclone size goog:{"$link"} 2>&1`
+check_results=`gclone size goog:{"$link"} --min-size 10M -q 2>&1`
 id=$link
-    j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
+    j=$(gclone lsd oneking:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
     if [[ $check_results =~ "Error 404" ]] ; then
     echo "链接无效，检查是否有权限" && exit
     else
@@ -19,13 +23,26 @@ id=$link
     echo "file name："$rootName""
     fi
 fi
-    echo "==<<极速转存即将开始，可ctrl+c中途中断>>=="
+echo "==<<极速转存即将开始，可ctrl+c中途中断>>=="
+copycommend() {
     echo 【开始拷贝】......
-    gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P
-    echo 【查缺补漏】......
-    gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P
+    gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P
+    echo "|>>>>>>>>>>>>>>>|100%  拷贝完毕"
     echo 【去重检查】......
-    gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -q -P
+    gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -q
+    echo "|>>>>>>>>>>>>>>>|100%  查重完毕"
+}
+dedupecommend
+checkcommend() {
     echo 【比对检查】......
-    gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --min-size 10M --stats-log-level NOTICE
+    c1=`gclone check goog:{$link} goog:{myid}/"$rootName" --size-only --one-way --no-traverse --min-size 10M -q`
+    if [[ "$c1"!="" ]] 
+    then 
+    echo "|>>>>>>>>>>>>>>>|100%  检查OK，转存已完成"
     ./gdbot.sh
+    break
+    fi
+    else
+    copycommend
+    checkcommend
+}
