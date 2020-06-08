7fcfb15e24aeafe7d6b953d3c7dff32e50169525
cgking <58586625+cgkings@users.noreply.github.com>
2020-06-08 20:20:31 +0800

    Update gdbot.sh

--- gdbot.sh
+++ gdbot.sh
@@ -8,30 +8,24 @@ read -p "请输入分享链接==>" link
 if [ -z "$link" ] ; then
     echo "不允许输入为空" && exit
 else
-link=${link#*id=};
-link=${link#*folders/};
-link=${link#*d/};
-link=${link%?usp*}
-id=$link
-    j=$(gclone lsd goog:{$id} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
+j=$(gclone ls goog:{$link} --dump bodies -vv 2>&1 | grep '^{"id"') rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
     if [[ "$j" =~ "Error 404" ]] ; then
     echo "链接无效，检查是否有权限" && exit
     else
-    echo "分享链接的基本信息如下："
-    echo "file name："$rootName""
+    echo "文件夹名称为："$rootName""
     fi
 fi
 echo "==<<极速转存即将开始，可ctrl+c中途中断>>=="
 echo 【开始拷贝】......
-gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P --stats = 0.2m
+gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
 echo 【查缺补漏】......
-gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q -P --stats = 0.2m
+gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M -q
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
 echo 【去重检查】......
 gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -q
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
 echo 【比对检查】......
-gclone check goog:{$link} goog:{myid}/"$rootName" --size-only --one-way --no-traverse --min-size 10M -q
+gclone check goog:{$link} goog:{myid}/"$rootName" --size-only --one-way --no-traverse --min-size 10M
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  检查完毕"
 ./gdbot.sh
