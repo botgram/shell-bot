c1d408b7f6c85972dfdfd36d5e9809cceb522e50
cgking <58586625+cgkings@users.noreply.github.com>
2020-06-30 14:38:34 +0800

    Update gdbot.sh

--- gdbot.sh
+++ gdbot.sh
@@ -13,7 +13,7 @@ link=${link#*folders/};
 link=${link#*d/};
 link=${link%?usp*}
 id=$link
-j=$(gclone ls goog:{$link} --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
+j=$(gclone lsd goog:{$link} --checkers=64 --dump bodies -vv 2>&1 | grep '^{"id"' | grep $id) rootName=$(echo $j | grep -Po '(?<="name":")[^"]*')
     if [[ "$j" =~ "Error 404" ]] ; then
     echo "链接无效，检查是否有权限" && exit
     else
@@ -28,7 +28,7 @@ echo 【查缺补漏】......
 gclone copy oneking:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs --transfers=20 --min-size 10M --log-file=/root/gclone_log/"$rootName"'_copy2.txt' -vP --stats-one-line --stats 0.1m
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  拷贝完毕"
 echo 【去重检查】......
-gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -q --log-file=/root/gclone_log/"$rootName"'_dedupe.txt'
+gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs --checkers=64 -q --log-file=/root/gclone_log/"$rootName"'_dedupe.txt'
 echo "|▉▉▉▉▉▉▉▉▉▉▉▉|100%  查重完毕"
 echo 【比对检查】......
 gclone check goog:{$link} goog:{myid}/"$rootName" --size-only --one-way --no-traverse --min-size 10M
