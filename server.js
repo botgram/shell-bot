c15306398ddb43a5d00372dc0ef64928a6f545c0
cgking <58586625+cgkings@users.noreply.github.com>
2020-07-03 21:29:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -513,7 +513,7 @@ bot.command("gd", function (msg, reply, next) {
   msg.editor = null;
 
 // command that that should be used
-  var args = "./gdbot.sh";
+  var args = "./fclone.sh";
   msg.context.command = new Command(reply, msg.context, args);
   msg.context.command.on("exit", function() {
     msg.context.command = null;
