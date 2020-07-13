f9774f46795e29458f1edb45e7a315f33417bb24
cgking <58586625+cgkings@users.noreply.github.com>
2020-07-13 08:54:21 +0800

    Update fptask.sh

--- script/fptask.sh
+++ script/fptask.sh
@@ -22,9 +22,9 @@ if [ -z "$link2" ] ; then
     echo "不允许输入为空" && exit
 else
 link2=${link2#*id=};link2=${link2#*folders/};link2=${link2#*d/};link2=${link2%?usp*}
-fi
 sed -i "s/${fp_link1}/${link1}/g" /root/fclone_shell_bot/myfc_config.ini
 sed -i "s/${fp_link2}/${link2}/g" /root/fclone_shell_bot/myfc_config.ini
+fi
 echo -e "/n该模式暂不支持队列任务，仅支持后台任务"
 tmux new -s fptask -d
 tmux send -t "fptask" '~/gclone_shell_bot/script/fpcopy.sh' Enter
