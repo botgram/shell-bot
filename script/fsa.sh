54e5c12fe3a3b6b49315f39591f0187aaef3d5fc
cgking <58586625+cgkings@users.noreply.github.com>
2020-07-24 11:03:40 +0800

    新增sa检测脚本

---
+++ script/fsa.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /root/fclone_shell_bot/myfc_config.ini
+stty erase '^H'
+echo 正常的sa无输出，错误的才有，注意你的CPU负载！开始！
+read -p "输入你的sa保存目录！ : " safolder
+mkdir -p $safolder/invalid
+echo running...
+find $safolder -type f -name "*.json" | xargs -I {} -n 1 -P 10 bash -c 'fclone lsd '$fclone_name':{'$gd_id'} --drive-service-account-file={} --drive-service-account-file-path=""  &> /dev/null || mv {} '$safolder'/invalid '
+echo alldone
+
+######safolder###                                                              ####remote#####            ###########33folderid###########          &> /dev/null
+#本地存放sa的目录##                                                          ##fclone config show 查看###   #######团队盘目录id############
+#######################请修改以上三个参数之后给脚本权限后执行脚本###############
--- script/fsigle.sh
+++ script/fsigle.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 source /root/fclone_shell_bot/myfc_config.ini
 clear
-read -p "请输入目标链接==>" link1
+read -p "请输入from 链接==>" link1
 link1=${link1#*id=};link1=${link1#*folders/};link1=${link1#*d/};link1=${link1%?usp*}
-read -p "请输入目的地链接==>" link2
+read -p "请输入copy to链接==>" link2
 link2=${link2#*id=};link2=${link2#*folders/};link2=${link2#*d/};link2=${link2%?usp*}
-for input_id in $(fclone lsf "$fclone_name":{$link1} --format "i" --files-only -R)
+fclone lsf "$fclone_name":{$link1} --format "pi" --files-only -R > /root/fclone_shell_bot/log/fsingle.txt
+IFS=$'\n'
+suma=0
+sumh=$(grep -n '' /root/fclone_shell_bot/log/fsingle.txt | awk -F : 'END{print $1}')
+for input_id in $(cat /root/fclone_shell_bot/log/fsingle.txt | cut -d ';' -f 2)
 do
-fclone copy "$fclone_name":{$input_id} "$fclone_name":{$link2} --fast-list --drive-server-side-across-configs --stats=1s --stats-one-line -vP --checkers="$fq_chercker" --transfers="$fq_transfer" --drive-pacer-min-sleep="$fq_min_sleep"ms --drive-pacer-burst="$fq_BURST" --min-size "$fq_min_size"M --check-first
+suma=$((suma+1))
+input_name=$(cat /root/fclone_shell_bot/log/fsingle.txt | grep '"'$input_id'"' | cut -d ';' -f 1)
+input_per=$(printf "%d%%" $((suma*100/sumh)))
+echo -e "▣▣▣▣▣▣▣任务信息▣▣▣▣▣▣▣\n" 
+echo -e "┋资源名称┋:"$input_name"\n"
+echo -e "┋资源地址┋:"$input_id"\n"
+echo -e "┋任务信息┋:第"$suma"项/共"$sumh"项\n"
+echo -e "┋任务进度┋:"$input_per""
+fclone copy "$fclone_name":{$input_id} "$fclone_name":{$link2} --drive-server-side-across-configs --fast-list --no-traverse --size-only --stats=1s --stats-one-line -q --drive-pacer-min-sleep=1ms --ignore-checksum --ignore-existing --buffer-size=100M --use-mmap
 done
 exit
